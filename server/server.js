f913c0674d623da46b4441b4e0eaa929b58c446c
ade5791 <adetola@live.com>
2020-07-25 17:24:02 -0400

    .

--- server/server.js
+++ server/server.js
@@ -1,15 +1,30 @@
 const path = require('path');
+const http = require('http')
 const express = require('express');
-const port = process.env.PORT || 5000;
+const socketIO = require('socket.io');
 
 const publicPath =  path.join(__dirname, '/../public');
 
+const port = process.env.PORT || 5000;
 
-
-var app = express();
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(express.static(publicPath))
 
-app.listen(port, () => {
+
+io.on('connection', (socket) =>{
+    console.log ("a new user just connected");
+
+    socket.on('disconnect', (socket) =>{
+        console.log ("User was disconnected")
+    
+    })
+})
+
+
+
+server.listen(port, () => {
     console.log(`server is up on port ${port}`)
 })
